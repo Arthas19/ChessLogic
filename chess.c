fd910975a18446c2d230f13826f3fdaaf0d5bbbf
Arthas19 <mpasic9697@gmail.com>
2019-05-03 17:53:44 +0200

    move_pawn

---
+++ chess.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Defined Variables */
+#define EMPTY 0
+#define WIDTH 8
+#define HEIGHT 8
+#define TABLE_SIZE 64
+
+/* Functions used */
+
+// Printing the chess table 
+void print_table();
+void move_pawn(int pos, int turn);
+void move_rook(int pos, int turn);
+void move_knight(int pos, int turn);
+void move_bishop(int pos, int turn);
+void move_queen(int pos, int turn);
+void move_king(int pos, int turn);
+
+/* Custom Structures used */
+
+/* Globar variables */
+// EMPTY  0    0
+// PAWN   1   -1
+// ROOK   5   -5
+// KNIGHT 3   -3
+// BISHOP 4   -4
+// QUEEN  20  -20
+// KING   100 -100
+
+// Black player  1 
+// White player -1
+static int player_turn = -1;
+static int chess_table[TABLE_SIZE] = { -5, -3, -4, -100, -20, -4, -3, -5,
+                                       -1, -1, -1,   -1,  -1, -1, -1, -1,
+                                        0,  0,  0,    0,   0,  0,  0,  0,
+                                        0,  0,  0,    0,   0,  0,  0,  0, 
+                                        0,  0,  0,    0,   0,  0,  0,  0, 
+                                        0,  0,  0,    0,   0,  0,  0,  0, 
+                                        1,  1,  1,    1,   1,  1,  1,  1,
+                                        5,  3,  4,  100,  20,  4,  3,  5 };
+
+int main() {
+
+	print_table();
+
+    return 0;
+}
+
+void print_table() {
+
+	//system("clear");
+
+	for ( int i = 0; i < TABLE_SIZE; i++ ) {
+		if ( i % 8 == 0 ) {
+			printf("\n");
+		}
+
+		printf(" %d", chess_table[i]);
+	}
+
+	printf("\n\n");
+}
+
+void move_pawn(int pos, int turn) {
+	int available[4] = { 0, 0, 0, 0 };
+
+	if ( chess_table[pos + WIDTH] == EMPTY ) {
+		available[0] = 1;
+	}
+
+	if ( chess_table[pos + WIDTH
+
+}
